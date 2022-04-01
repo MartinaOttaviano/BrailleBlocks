e1736017fab51adabfa7c9dc18c65f65635e9530
Martina Ottaviano <martina99ottaviano@gmail.com>
2022-04-01 15:29:51 +0200

    Minor changes

--- Animation_Test/CompleteView.swift
+++ Animation_Test/CompleteView.swift
@@ -10,25 +10,28 @@ import SwiftUI
 
 struct CompleteView: View {
     var body: some View{
+        NavigationView{
         VStack{
+            
             ScrollView(.horizontal, showsIndicators: false){
                 VStack{
                     
                     Image("braille_string")
                         .frame(width: 1450, height: 80)
-                        .opacity(0.6)
+//                        .opacity(0.6)
                     
                     Text("The rabbit hopped on the chair")
                         .frame(width: 1450, height: 80,alignment: .center)
                         .font(.custom("Menlo", size: 80))
                         .padding(.horizontal,30)
-                        .opacity(0.6)
+                        .opacity(0.4)
                     
                     
                     Image("string_color")
                         .frame(width: 1450, height: 80)
                     
-                }.padding(.vertical,120)
+                }
+                .padding(.bottom,160)
                 
             }
             
@@ -41,39 +44,42 @@ struct CompleteView: View {
                         print("Try again")
                     }, label: {
                         
-                        ZStack{
-                            Rectangle()
-                                .frame(width: 150, height: 60)
-                                .cornerRadius(20)
-                                .foregroundColor(Color.init(red: 254/255, green: 191/255, blue: 0/255))
+                            ZStack{
+                                Rectangle()
+                                    .frame(width: 150, height: 60)
+                                    .cornerRadius(20)
+                                    .foregroundColor(Color.init(red: 254/255, green: 191/255, blue: 0/255))
+                                    
+                                Text("Try again")
+                                    .font(.system(size: 30))
+                                    .foregroundColor(.black)
+                            }
+                    }).padding(.horizontal,10)
+                        
+                        
+                        Button(action: {
+                            print("Next practice")
+                        }, label: {
                             
-                            Text("Try again")
-                                .font(.system(size: 30))
-                                .foregroundColor(.black)
-                        }
-                    }).padding(.horizontal,10)
-                    
-                    
-                    Button(action: {
-                        print("Go back")
-                    }, label: {
+                                ZStack{
+                                    Rectangle()
+                                        .frame(width: 150, height: 60)
+                                        .cornerRadius(20)
+                                        .foregroundColor(Color.init(red: 254/255, green: 191/255, blue: 0/255))
+                                    Text("Next")
+                                        .font(.system(size: 30))
+                                        .foregroundColor(.black)
+                                }
+                            
+                            
+                        }).padding(.horizontal,10)
                         
-                        ZStack{
-                            Rectangle()
-                                .frame(width: 150, height: 60)
-                                .cornerRadius(20)
-                                .foregroundColor(Color.init(red: 254/255, green: 191/255, blue: 0/255))
-                            Text("Go back")
-                                .font(.system(size: 30))
-                                .foregroundColor(.black)
-                        }
-                        
-                        
-                    }).padding(.horizontal,10)
-                    
                     
                 }
             }
+//            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height*0.35,alignment: .center)
+            } .navigationTitle("")
+                
         }
     }
 }

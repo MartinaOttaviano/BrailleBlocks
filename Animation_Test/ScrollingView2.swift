38702d0bc8f427f831535344e5e126937c4271ab
Martina Ottaviano <martina99ottaviano@gmail.com>
2022-04-01 16:21:00 +0200

    Added text highlighting

--- Animation_Test/ScrollingView2.swift
+++ Animation_Test/ScrollingView2.swift
@@ -9,30 +9,31 @@ import Foundation
 import SwiftUI
 
 struct ScrollingView2: View {
+    @State private var tabTag: Int = 0
     let letters: [String] = ["T","H","E","_","R","A","B","B","I","T","_","H","O","P","P","E","D","_","O","N","_","T","H","E","_","C","H","A","I","R"]
     var body: some View{
         NavigationView{
         VStack{
             
-        TabView{
-            ForEach(0..<29) {i in
+            TabView(selection: $tabTag){
                 ForEach(0..<letters.count, id:\.self) { num in
-                VStack{
-                    BrailleView()
-//                Text("T")
-                    Text("**\(letters[num])**")
-                    .font(.custom("Menlo", size: 50))
-                    .foregroundColor(.black)
+                    VStack{
+                        BrailleView()
+                        //                Text("T")
+                        Text("**\(letters[num])**")
+                            .font(.custom("Menlo", size: 50))
+                            .foregroundColor(.black)
+                    }
+                    .tag(num)
                 }
-                }
-            }
-        }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height*0.5)
+            }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height*0.5)
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             
             
-        Text("The rabbit hopped on the chair")
-            .opacity(0.5)
-            .font(.system(size: 22))
+//        Text(frase)
+//            .opacity(0.5)
+//            .font(.system(size: 22))
+            Frase(tabTag: $tabTag)
             
        
         Button(action: {
@@ -73,6 +74,26 @@ struct ScrollingView2: View {
         
     }
 }
+
+
+struct Frase: View{
+    @Binding var tabTag: Int
+    var body: some View{
+        let frase = "The rabbit hopped on the chair"
+        HStack(spacing: 0){
+            ForEach(0..<frase.count, id:\.self){ index in
+                Text(String(frase[frase.index(frase.startIndex, offsetBy: index)]))
+                    .opacity(index == tabTag ? 1 : 0.5)
+                    .font(.system(size: 22).weight(index == tabTag ? .bold : .regular))
+                    .foregroundColor(index == tabTag ? .blue : .black)
+                    
+            }
+        }
+        
+    }
+}
+
+
 struct ScrollingView2_Previews: PreviewProvider {
     static var previews: some View {
         ScrollingView2()
